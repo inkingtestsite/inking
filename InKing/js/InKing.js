480e5c7a1014b53a8b9d28d69984f82b6deae4a1
inkingtestsite <76078951+inkingtestsite@users.noreply.github.com>
2020-12-15 20:55:55 -0400

    Add files via upload

---
+++ InKing/js/InKing.js
@@ -0,0 +1,24 @@
+function toggle(){
+  var x = document.getElementById("open-hours");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else{
+    x.style.display = "none";
+  }
+}
+
+function upDate(previewPic){
+    document.getElementById('image').style.backgroundColor = "#ffffff00";
+    document.getElementById('image').style.backgroundImage = "url('"+ previewPic.src +"')";
+
+    document.getElementById('image').innerHTML = previewPic.alt;
+  
+  }
+
+function unDo(){
+   document.getElementById('image').style.backgroundColor = "#ffffff00";
+     document.getElementById('image').style.backgroundImage = "url('')";
+     
+     document.getElementById('image').innerHTML = "Hover over an image below to display here";  
+
+  }
