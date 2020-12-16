792a555cbc81f953cb6fde7f8f7da15ae13310b4
inkingtestsite <76078951+inkingtestsite@users.noreply.github.com>
2020-12-16 14:41:45 -0400

    Add files via upload

--- InKing/js/InKing.js
+++ InKing/js/InKing.js
@@ -11,7 +11,7 @@ function upDate(previewPic){
     document.getElementById('image').style.backgroundColor = "#ffffff00";
     document.getElementById('image').style.backgroundImage = "url('"+ previewPic.src +"')";
 
-    document.getElementById('image').innerHTML = previewPic.alt;
+    document.getElementById('image').innerHTML = "previewPic.alt";
   
   }
 
@@ -19,6 +19,6 @@ function unDo(){
    document.getElementById('image').style.backgroundColor = "#ffffff00";
      document.getElementById('image').style.backgroundImage = "url('')";
      
-     document.getElementById('image').innerHTML = "Hover over an image below to display here";  
+     document.getElementById('image').innerHTML = "Hover over, or tab to an image in the gallery to enlarge here.";  
 
   }
